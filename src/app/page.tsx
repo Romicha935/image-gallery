6ab4df69531b255144ef79993cee2551c4e81b73
romicha-parvin <romichaparvin35@gmail.com>
2025-06-28 18:17:47 +0600

    image gallery added

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <main className='flex  justify-center items-center min-h-screen bg-sky-200'>
+    <div className='w-full max-x-screen-2xl mx-auto px-20 bg-slate-100 rounded-2xl shadow-lg'>
+    <h1 className='text-green-500 text-4xl font-bold'>Home page</h1>
+    </div>
+    </main>
+  )
+}
+
+export default HomePage
